1e4c39bb5b8ac831a95550c784c7447827f3590a
rongying <kohrongying@gmail.com>
2021-04-06 00:52:52 +0800

    Silence aws cli output.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,11 @@ install_zip_dependencies(){
 }
 
 publish_dependencies_as_layer(){
-	echo "Publishing dependencies as a layer..."
+	echo "\nPublishing dependencies as a layer..."
 	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
 	LAYER_VERSION=$(jq '.Version' <<< "$result")
+    echo "Publish lambda layer successful!"
+    echo "Lambda Layer Version: $LAYER_VERSION"
 	rm -rf python
 	rm dependencies.zip
 }
@@ -26,16 +28,22 @@ files_to_exclude() {
 }
 
 publish_function_code(){
-	echo "Deploying the code itself..."
+	echo "\nDeploying the code itself..."
     files_to_exclude
 	zip -r code.zip . -x@exclude.lst
-	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Deploy lambda successful!"
+    fi
     rm exclude.lst
 }
 
 update_function_layers(){
-	echo "Using the layer in the function..."
-	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+	echo "\nUsing the layer in the function..."
+	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}" > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Update lambda layer successful!"
+    fi
 }
 
 deploy_lambda_function(){
